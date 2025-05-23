06e09bdbf14b45abf7202d9f1ea9e43f6fc5d4c3
anjeljayswal <anjeljayswal@gmail.com>
2025-05-23 13:46:30 +0530

    get api setup done

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("I am in the exppress server");
+// console.log("I am in the exppress server");
+const express = require("express");
+const dotenve = require("dotenv").config();
+const app = express();;
+const port = process.env.PORT || 5000;
+
+app.get("/api/contacts", (req, res) => {
+    // res.send({"message":"Get all constcts"}) // this is the old way
+    // res.json({"message":"Get all constcts"}) // this is the new way
+    res.status(200).json({ "message": "Get all contacts" }) // this is the new way
+})
+
+
+app.listen(port, () => { console.log(`server is running on port ${port}`) });
