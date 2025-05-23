04533611eaa27a1e6f42a888a5c578bd3c673557
anjeljayswal <anjeljayswal@gmail.com>
2025-05-23 15:43:22 +0530

    ayncHandler used to handle error

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,44 +1,47 @@
+const asyncHandler = require('express-async-handler');
+//ayncHandler is a middleware that will handle the error for us we dont have to use try and catch block for every function
 //@desc Get all Contacts
 //@route GET /api/contacts
 //@access Public
-const getContacts = (req,res)=>{
-    res.status(200).json({"message":"Get all Contacts"});
-};
+const getContacts = asyncHandler(async (req, res) => {
+    res.status(200).json({ "message": "Get all Contacts" });
+});
 
 //@desc Get a Contact
-//@route Get /api/contacts/:id
+//@route Get /api/contac ts/:id
 //@access Public
-const getContact = (req,res)=>{
-    res.status(200).json({message:`Get a Contact for ${req.params.id}`});
-}
+const getContact = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Get a Contact for ${req.params.id}` });
+})
 
 //@desc Create new Contact
 //@route POST /api/contacts
 //@access Public
-const createContact = (req,res)=>{
-    console.log("the request body is:",req.body); //it will we give undefine we have to use ecpress middleware for json onject which we can get from the body
-    const {name,email,phone} = req.body;
-    if(!name || !email || !phone){
+const createContact = asyncHandler(async (req, res) => {
+    console.log("the request body is:", req.body); //it will we give undefine we have to use ecpress middleware for json onject which we can get from the body
+    const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {
         res.status(400);
         throw new Error("Please include all fields");
-        
+
     }
-    res.status(201).json({message: "Create Contacts"});
-}
+    res.status(201).json({ message: "Create Contacts" });
+})
 
 //@desc Update a Contact
 //@route PUt /api/contacts/:id
 //@access Public
-const updateContact =(req,res)=>{
-    res.status(200).json({message:`Update Contacts for ${req.params.id}`});
-}
+const updateContact = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Update Contacts for ${req.params.id}` });
+})
 //@desc Delet a Contact
 //@route DELETE /api/contacts/:id
 //@access Public
-const deleteContact =(req,res)=>{
-    res.status(200).json({message:`Delete Contacts for ${req.params.id}`});
-}
+const deleteContact = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Delete Contacts for ${req.params.id}` });
+})
 
 
 module.exports = {
-    getContact, createContact,updateContact,getContacts,deleteContact  }
+    getContact, createContact, updateContact, getContacts, deleteContact
+}
