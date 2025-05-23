da4abafb920201a63fbd0ae227cc56dfecea41e1
anjeljayswal <anjeljayswal@gmail.com>
2025-05-23 14:04:36 +0530

    created all routes and checked in postman

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -4,5 +4,16 @@ const router = express.Router();
 router.route("/").get((req,res)=>{
     res.status(200).json({"message":"Get all Contacts"});
 })
-
+router.route("/:id").get((req,res)=>{
+    res.status(200).json({message:`Get a Contact for ${req.params.id}`});
+})
+router.route("/").post((req,res)=>{
+    res.status(200).json({"message":"Create Contacts"});
+})
+router.route("/:id").put((req,res)=>{
+    res.status(200).json({message:`Update Contacts for ${req.params.id}`});
+})
+router.route("/:id").delete((req,res)=>{
+    res.status(200).json({message:`Delete Contacts for ${req.params.id}`});
+})
 module.exports = router;
