15730a6c4b602d621496fc94df17cf5d806f9b52
anjeljayswal <anjeljayswal@gmail.com>
2025-05-23 13:57:14 +0530

    routes folder created and succesfully get api created

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const dotenve = require("dotenv").config();
 const app = express();;
 const port = process.env.PORT || 5000;
 
-app.get("/api/contacts", (req, res) => {
-    // res.send({"message":"Get all constcts"}) // this is the old way
-    // res.json({"message":"Get all constcts"}) // this is the new way
-    res.status(200).json({ "message": "Get all contacts" }) // this is the new way
-})
+// app.get("/api/contacts", (req, res) => {
+//     // res.send({"message":"Get all constcts"}) // this is the old way
+//     // res.json({"message":"Get all constcts"}) // this is the new way
+//     res.status(200).json({ "message": "Get all contacts" }) // this is the new way
+// })
+
+app.use("/api/contacts", require("./routes/contactRoutes"));
 
 
 app.listen(port, () => { console.log(`server is running on port ${port}`) });
